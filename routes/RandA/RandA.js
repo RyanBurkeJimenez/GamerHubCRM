f8139f2e933fa51a465a40f1c5c4232676912b2c
RyanBurkeJimenez <rburkeji@asu.edu>
2024-04-13 04:25:36 -0700

    Completed the RandA function of the API. Added an image to simulate graph data

--- routes/RandA/RandA.js
+++ routes/RandA/RandA.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const Customer = require('../../models/customer')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.render('RandA/RandA')
+router.get('/', async (req,res)=>{
+    try{
+        const customers = await Customer.find()
+    res.render('RandA/RandA',{customers})
+}
+    catch(error){
+        console.error('faild to load in Inventory Data', error)
+        res.status(500).send('Internal Server error')
+    }
 })
 
 module.exports = router;
